0bc448c95dc59f90bd317894b12b3490374a4062
Jahanzaib Tayyab <jtayyab204@gmail.com>
2023-10-15 19:10:19 +0500

    update dashboard page

--- nextJs/next-session-8-oct/src/app/dashboard/page.tsx
+++ nextJs/next-session-8-oct/src/app/dashboard/page.tsx
@@ -1,11 +1,5 @@
-import { Button } from "@/components/ui/button";
 const Dashboard = () => {
-  return (
-    <div>
-      This is Dashboard Route
-      <Button variant="destructive">Shadcn UI</Button>
-    </div>
-  );
+  return <div>This is Dashboard Route</div>;
 };
 
 export default Dashboard;
